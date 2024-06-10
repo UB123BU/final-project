7a26a30e703f2377a2783703f36cae586a2fec33
UB123BU <161341513+UB123BU@users.noreply.github.com>
2024-06-10 19:33:11 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    double BMI;
-    if (args.length == 2) {
-        double wzrost = Double.parseDouble(args[0]);
-        double waga = Double.parseDouble(args[1]);
-        BMI = waga / ((wzrost / 100) * (wzrost / 100));
-        System.out.println("Twój wskaźnik BMI to: " + BMI);
-    } else {
-        System.out.println("Please provide wzrost and waga as arguments.");
-    }}}
+    double wzrost = 176
+    double waga = 80
+    double BMI = waga / ( (wzrost/100)*(wzrost/100))
+    System.out.println("BMI wynosi: " + BMI)
