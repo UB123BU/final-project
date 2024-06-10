f5920affdba3ed5cddd9689aa8183369739801d7
UB123BU <161341513+UB123BU@users.noreply.github.com>
2024-06-10 19:36:24 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    double wzrost = 176
-    double waga = 80
-    double BMI = waga / ( (wzrost/100)*(wzrost/100))
-    System.out.println("BMI wynosi: " + BMI)
+    double wzrost = 170;
+    double waga = 80;
+    double BMI = (waga / ((wzrost / 100) * (wzrost / 100)));
+System.out.println("Twój wskaźnik BMI to:" + BMI);
+  }}
