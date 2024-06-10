19c1ff8de5bfe77e724712326db30fbd86e5e3b7
UB123BU <161341513+UB123BU@users.noreply.github.com>
2024-06-10 20:35:37 +0200

    Update main.java

--- main.java
+++ main.java
@@ -4,5 +4,4 @@ public class Main {
     double waga = 80;
     double BMI = (waga / ((wzrost / 100) * (wzrost / 100)));
 System.out.println("Twój wskaźnik BMI to:" + BMI);
-    emailext body: "Twój wskaźnik BMI to: $BMI",          subject: "Wynik BMI",          to: "k.kapitula.063@studms.ug.edu.pl";
   }}
