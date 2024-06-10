2dfab9acbf3b9ee3b50e058843d596dcc74b7588
UB123BU <161341513+UB123BU@users.noreply.github.com>
2024-06-10 03:38:15 +0200

    Update main.java

--- main.java
+++ main.java
@@ -16,3 +16,32 @@ public class Main {
     System.out.printf("Twój wskaźnik BMI to: %1$s \r\n", BMI);
   }
 }
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class BMICalculatorTest {
+    @Test
+    void testCalculateBMI_1() {
+        double wzrost = 175;
+        double waga = 70;
+        double expectedBMI = 22.86;
+        double BMI = BMI(wzrost, waga);
+        assertEquals(expectedBMI, BMI, 0.01);
+    }
+    @Test
+    void testCalculateBMI_2() {
+        double wzrost = 165;
+        double waga = 50;
+        double expectedBMI = 18.36;
+        double BMI = BMI(wzrost, waga);
+        assertEquals(expectedBMI, BMI, 0.01);
+    }
+    @Test
+    void testCalculateBMI_3() {
+        double wzrost = 180;
+        double waga = 90;
+        double expectedBMI = 27.78;
+        double BMI = BMI(wzrost, waga);
+        assertEquals(expectedBMI, BMI, 0.01);
+    }
+    }
