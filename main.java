526deb72189068771e2301a9c2797cf70a89d96b
UB123BU <161341513+UB123BU@users.noreply.github.com>
2024-06-10 18:10:40 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,18 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
   public static void main(String[] args) {
-    
-    System.out.println("Podaj wzrost w cm:");
-    int wzrost = Integer.parseInt(new Scanner(System.in).nextLine());
-
-    System.out.println("Podaj wage w kg:");
-    int waga = Integer.parseInt(new Scanner(System.in).nextLine());
-  }
-
-  private static void wyliczBMI(double wzrost, double waga)
+    if (args.length == 2) {
+        double wzrost = Double.parseDouble(args[0]);
+        double waga = Double.parseDouble(args[1]);
+        wyliczBMI(wzrost, waga);
+    } else {
+        System.out.println("Please provide wzrost and waga as arguments.");
+    }
+    wyliczBMI(double wzrost, double waga)
   {
     double BMI = waga / ((wzrost / 100) * (wzrost / 100));
     System.out.printf("Twój wskaźnik BMI to: %1$s \r\n", BMI);
   }
+}
 }
